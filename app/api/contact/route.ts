3ecb2bddfd35d5d722b5a0a270aaa7ffadc9a763
Ermias <35461600+ermiaskidane@users.noreply.github.com>
2024-01-06 14:41:10 +0000

    comment nodemailer

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -14,42 +14,42 @@ export const POST = async (req: Request) => {
     // to format the html we can use the react-email component library 
     // both in resend and nodemailer html property
 
-    // const data = await resend.emails.send({
-    //   from: 'LeospaBeauty <onboarding@resend.dev>',
-    //   to: ["sotyu28@gmail.com"],
-    //   subject:
-    //     'Thanks for your order! This is your receipt.',
-    //   html: `<div>${message}</div><p>Sent from: ${email}</p>`,
-    // })
+    const data = await resend.emails.send({
+      from: 'LeospaBeauty <hello@ermiaskidane.online>',
+      to: ["sotyu28@gmail.com"],
+      subject:
+        'Thanks for your order! This is your receipt.',
+      html: `<div>${message}</div><p>Sent from: ${email}</p>`,
+    })
 
     // send message using nodemailer intergated with resend
-    const transport = nodemailer.createTransport({
-      host: "smtp.resend.com",
-      secure: true,
-      port: 465,
-      auth: {
-        // user: process.env.EMAIL_USERNAME, // generated ethereal user
-        user: "resend",
-        pass: process.env.RESEND_API_KEY
-      }
-    })
+    // const transport = nodemailer.createTransport({
+    //   host: "smtp.resend.com",
+    //   secure: true,
+    //   port: 465,
+    //   auth: {
+    //     // user: process.env.EMAIL_USERNAME, // generated ethereal user
+    //     user: "resend",
+    //     pass: process.env.RESEND_API_KEY
+    //   }
+    // })
 
 
-    const mailData = {
-      from: 'LeospaBeauty <hello@ermiaskidane.online>',
-      to: 'sotyu28@gmail.com',
-      subject: `Message From ${firstName}`,
-      text: message + " | Sent from: " + email,
-      html: `<div>${message}</div><p>Sent from:
-      ${email}</p>`
-    }
+    // const mailData = {
+    //   from: 'LeospaBeauty <hello@ermiaskidane.online>',
+    //   to: 'sotyu28@gmail.com',
+    //   subject: `Message From ${firstName}`,
+    //   text: message + " | Sent from: " + email,
+    //   html: `<div>${message}</div><p>Sent from:
+    //   ${email}</p>`
+    // }
 
-    transport.sendMail(mailData, function (err, info) {
-      if(err)
-        console.log(err)
-      else
-        console.log(info)
-    })
+    // transport.sendMail(mailData, function (err, info) {
+    //   if(err)
+    //     console.log(err)
+    //   else
+    //     console.log(info)
+    // })
 
     // res.status(200)
     return NextResponse.json("message sent");
